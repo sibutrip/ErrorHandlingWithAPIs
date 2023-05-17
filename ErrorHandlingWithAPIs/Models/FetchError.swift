3e429891e809bfb24246e91b7f431a90ec4142f2
Zoe Cutler <36316793+zcutlerf@users.noreply.github.com>
2023-05-17 16:12:44 -0400

    lol pwned

--- ErrorHandlingWithAPIs/Models/FetchError.swift
+++ ErrorHandlingWithAPIs/Models/FetchError.swift
@@ -10,6 +10,7 @@ import Foundation
 enum FetchError {
     case urlSessionError(Error)
     case invalidJSONResponse
+    case zoeMadeAPullRequestEvenThoughCorySaidNo
 }
 
 extension FetchError: LocalizedError {
@@ -19,6 +20,8 @@ extension FetchError: LocalizedError {
             return NSLocalizedString("Could not process url request", comment: "Fetch Error")
         case .invalidJSONResponse:
             return NSLocalizedString("Could not read JSON response", comment: "JSON Decoding Error")
+        case .zoeMadeAPullRequestEvenThoughCorySaidNo:
+            return NSLocalizedString("Zoe made a PR even though Cory said no", comment: "Lol pwned")
         }
     }
 }
