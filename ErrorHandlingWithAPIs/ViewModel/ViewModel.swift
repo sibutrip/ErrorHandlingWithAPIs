a65b364c5aa6c679acfad1cd1325f61a89bcbc15
Cory Tripathy <ctripathy0@gmail.com>
2023-05-17 09:26:45 -0400

    add vm comments

--- ErrorHandlingWithAPIs/ViewModel/ViewModel.swift
+++ ErrorHandlingWithAPIs/ViewModel/ViewModel.swift
@@ -13,15 +13,16 @@ class ViewModel: ObservableObject {
     @Published var fetchError: FetchError?
     @Published var urlPath: String = ""
     
+    
     func fetchPlanets() async throws {
         
         guard let url = URL(string: "https://swapi.dev/api/\(urlPath.lowercased())") else {
             return
         }
         
-        /// a function marked with "try" throws errors. safely handle the error by
-        /// 1) catching it and
-        /// 2) setting our published fetchError to one of the cases of our FetchError enum
+        /// the following 2 functions marked with "try" throw errors. safely handle those errors by
+        /// 1) catching each of them with a do block and a catch block
+        /// 2) in each catch block, setting our published fetchError to one of the cases of our FetchError enum
         let (data, _) = try! await URLSession.shared.data(from: url)
         let decoder = JSONDecoder()
         let decodedData = try! decoder.decode(Response.self, from: data)
